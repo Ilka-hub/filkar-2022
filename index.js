73e1e813e59f260e88bd74100da4f2e7e1cf7d89
Ilka-hub <113768280+Ilka-hub@users.noreply.github.com>
2022-10-03 21:18:20 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+import * as THREE from './three.js-master/build/three.module.js'
+import {GLTFLoader} from './three.js-master/examples/jsm/loaders/GLTFLoader.js'
+
+const canvas = document.querySelector('.webgl')
+const scene = new THREE.Scene()
+
+const loader = new GLTFLoader()
+loader.load('assets/GLB_3_Coffeeman.glb', function(glb){
+    console.log(glb)
+    const root = glb.scene;
+    scene.add(root);
+}, function(xhr){
+    console.log((xhr.loaded/xhr.total * 100) + "% loaded")
+}, function(error){
+    console.log('Ein Fehler liegt vor')
+})
+const light = new THREE.DirectionalLight(0xffffff, 1)
+light.position.set(2,2,5)
+scene.add(light)
+
+// Boiler Plate Code
+const size = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+}
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.hight, 0.1, 100)
+camera.position.set(0,1,2)
+scene.add(camera)
+
+const renderer = new THREE.WebGL1Renderer({
+    canvas: canvas
+})
+
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.shadowMap.enabled = true
+renderer.gammaOutput = true
+
+function animate(){
+    requestAnimationFrame(animate)
+    renderer.render(scene,camera)
+}
+
+animate(0)
+
+
